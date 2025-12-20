0303ed3db4746e1429e0bc05b3147f00f7d44ee2
Thrillcrazyer <53187684+Thrillcrazyer@users.noreply.github.com>
2025-12-20 08:33:51 +0000

    Deploying to gh-pages from @ AIDoheeKim/AIDoheeKim.github.io@4e02125d329cfc534c1a9e5f86c379889dcfbe5e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,8 +17,8 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "A showcase of DOLab&#39;s projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -69,52 +69,10 @@ ninja.data = [{
       },{id: "news-data-optimization-lab-dolab-officially-opened-on-december-22-2024-we-are-excited-to-begin-our-research-journey-in-data-driven-decision-making-and-optimization-at-changwon-national-university",
           title: '🎉 Data Optimization Lab (DOLab) officially opened on December 22, 2024! We are...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
+          section: "News",},{id: "projects-",
+          title: '',
+          description: "감귤 가격 및 출하량 예측 모델 개발",
+          section: "Projects",},{
         id: 'social-cv',
         title: 'CV',
         section: 'Socials',
